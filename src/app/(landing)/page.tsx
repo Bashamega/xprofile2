8ce8a7b97e3575f9f35f03644e18397eb988270c
Bashamega <adambashaahmednaji@gmail.com>
2024-08-17 13:05:13 +0300

    Landing

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,13 @@
 import { auth } from "~/server/auth";
 import { Hero } from "./components/hero";
+import { Feat } from "./components/feat";
 
 export default async function HomePage() {
   const session = await auth();
   return (
     <main>
       <Hero />
+      <Feat/>
     </main>
   );
 }
