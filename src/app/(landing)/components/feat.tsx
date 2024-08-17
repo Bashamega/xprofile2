98f4d055a52d10bdea4e59cc0a67f47b4ab619f4
Bashamega <adambashaahmednaji@gmail.com>
2024-08-17 13:05:04 +0300

    feat section

---
+++ src/app/(landing)/components/feat.tsx
@@ -0,0 +1,13 @@
+import { SectionTitle } from "./sectionTitle";
+
+export function Feat() {
+  return (
+    <section className="w-screen mt-10">
+      <SectionTitle
+        title="Powerful features"
+        subTitle="for developers like you"
+        description="xProfile is more than just a link to your bio. We've built a comprehensive range of powerful features that elevate your professional profile."
+      />
+    </section>
+  );
+}
